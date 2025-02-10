f4c3e523d63e73fd27366e77f366d3a90425a14d
dario-mr <dario.mauri9@gmail.com>
2025-02-10 16:11:07 +0100

    Minor refactor

--- src/main/java/com/dario/ast/event/RefreshCurlPreviewEvent.java
+++ src/main/java/com/dario/ast/event/RefreshCurlPreviewEvent.java
@@ -4,9 +4,9 @@ import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.ComponentEvent;
 import com.vaadin.flow.component.button.Button;
 
-public class RefreshPreviewEvent extends ComponentEvent<Component> {
+public class RefreshCurlPreviewEvent extends ComponentEvent<Component> {
 
-    public RefreshPreviewEvent() {
+    public RefreshCurlPreviewEvent() {
         super(new Button(), false);
     }
 }
