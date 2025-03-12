4a9279bbabea0e9fa4a652c34721c9ee730709c2
dario-mr <dario.mauri9@gmail.com>
2025-03-12 17:47:26 +0100

    Minor UI adjustments

--- src/main/java/com/dario/ast/view/component/headline/Headline.java
+++ src/main/java/com/dario/ast/view/component/headline/Headline.java
@@ -1,11 +1,11 @@
 package com.dario.ast.view.component.headline;
 
-import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.H3;
 
-public class Headline extends H2 {
+public class Headline extends H3 {
 
-    public Headline() {
-        setText("API Stress Test");
-        addClassName("headline");
-    }
+  public Headline() {
+    setText("API Stress Test");
+    addClassName("headline");
+  }
 }
