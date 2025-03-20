a68919ff4ad608eca1fcd42ff4ebcda046f01e82
dario-mr <dario.mauri9@gmail.com>
2025-03-20 14:33:40 +0100

    Fix: regenerate cURL preview when an environment is selected

--- src/main/java/com/dario/ast/view/component/config/PreviewTextArea.java
+++ src/main/java/com/dario/ast/view/component/config/PreviewTextArea.java
@@ -5,16 +5,15 @@ import com.vaadin.flow.component.textfield.TextArea;
 
 public class PreviewTextArea extends TextArea {
 
-    public PreviewTextArea() {
-        setLabel("cURL preview");
+  public PreviewTextArea() {
+    setWidthFull();
+    setReadOnly(true);
+    getStyle()
+        .set("padding", "0")
+        .set("font-family", "monospace")
+        .set("font-size", "0.9em");
 
-        setWidthFull();
-        setReadOnly(true);
-        getStyle()
-                .set("padding", "0")
-                .set("font-family", "monospace")
-                .set("font-size", "0.9em");
+    getElement().addEventListener("click", e -> SuccessNotification.show("Copied to clipboard!"));
+  }
 
-        getElement().addEventListener("click", e -> SuccessNotification.show("Copied to clipboard!"));
-    }
 }
