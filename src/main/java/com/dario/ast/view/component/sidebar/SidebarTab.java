fd069c9b64b4227377c7f80991c3afe983810c65
dario-mr <dario.mauri9@gmail.com>
2025-05-18 21:15:57 +0200

    Re-add title to sidebar tabs

--- src/main/java/com/dario/ast/view/component/sidebar/SidebarTab.java
+++ src/main/java/com/dario/ast/view/component/sidebar/SidebarTab.java
@@ -2,21 +2,24 @@ package com.dario.ast.view.component.sidebar;
 
 import static com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment.CENTER;
 
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 
 public class SidebarTab extends VerticalLayout {
 
-  public SidebarTab(VaadinIcon vaadinIcon, String tooltip) {
+  public SidebarTab(VaadinIcon vaadinIcon, String tabTitle) {
     addClassName("main-sidebar-tab");
     setAlignItems(CENTER);
     setJustifyContentMode(JustifyContentMode.CENTER);
     setSpacing(false);
 
     var icon = vaadinIcon.create();
-    icon.setTooltipText(tooltip);
 
-    add(icon);
+    var title = new Span(tabTitle);
+    title.addClassName("main-sidebar-title");
+
+    add(icon, title);
   }
 
 }
