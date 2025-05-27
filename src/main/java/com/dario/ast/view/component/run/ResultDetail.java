14e15f01b80ac56838c09b542a1c0453607857a4
dario-mr <dario.mauri9@gmail.com>
2025-05-27 14:12:59 +0200

    Tweak result detail dialog

--- src/main/java/com/dario/ast/view/component/run/ResultDetail.java
+++ src/main/java/com/dario/ast/view/component/run/ResultDetail.java
@@ -2,19 +2,22 @@ package com.dario.ast.view.component.run;
 
 import static com.dario.ast.util.JsonUtil.prettifyJson;
 import static com.vaadin.flow.component.icon.VaadinIcon.CLOSE_CIRCLE;
-import static com.vaadin.flow.component.orderedlayout.FlexComponent.JustifyContentMode.END;
+import static com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment.CENTER;
+import static com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment.STRETCH;
+import static com.vaadin.flow.component.orderedlayout.FlexComponent.JustifyContentMode.BETWEEN;
 
 import com.dario.ast.core.domain.IndexedApiResponse;
 import com.dario.ast.proxy.api.dto.ApiResponse;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.dialog.Dialog;
-import com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment;
+import com.vaadin.flow.component.html.H4;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextArea;
 import com.vaadin.flow.component.textfield.TextField;
 
+@CssImport(value = "./styles/result-detail.css")
 @CssImport(value = "./styles/vaadin-dialog-overlay.css", themeFor = "vaadin-dialog-overlay")
 public class ResultDetail extends Dialog {
 
@@ -24,34 +27,34 @@ public class ResultDetail extends Dialog {
 
     var response = indexedResponse.response();
 
-    var closeButton = buildCloseButton();
-    var infoRow = buildInfoRow(indexedResponse.index(), response);
+    var titleRow = buildTitleRow(indexedResponse.index());
+    var infoRow = buildInfoRow(response);
     var responseBody = buildResponseBody(response);
 
-    var container = buildContainer(closeButton, infoRow, responseBody);
+    var container = buildContainer(titleRow, infoRow, responseBody);
+    container.addClassName("result-detail");
 
     add(container);
   }
 
-  private HorizontalLayout buildCloseButton() {
+  private HorizontalLayout buildTitleRow(int index) {
+    var title = new H4("Result #%s".formatted(index));
+
     var closeButton = new Button(CLOSE_CIRCLE.create());
     closeButton.addClickListener(event -> close());
     closeButton.getStyle().set("margin", "0");
 
-    var closeButtonRow = new HorizontalLayout(closeButton);
-    closeButtonRow.setWidthFull();
-    closeButtonRow.setJustifyContentMode(END);
-    closeButtonRow.setPadding(false);
-    closeButtonRow.setSpacing(false);
+    var titleRow = new HorizontalLayout(title, closeButton);
+    titleRow.setWidthFull();
+    titleRow.setJustifyContentMode(BETWEEN);
+    titleRow.setAlignItems(CENTER);
+    titleRow.setPadding(false);
+    titleRow.setSpacing(false);
 
-    return closeButtonRow;
+    return titleRow;
   }
 
-  private HorizontalLayout buildInfoRow(int responseIndex, ApiResponse response) {
-    var indexText = new TextField("#", String.valueOf(responseIndex), "");
-    indexText.setReadOnly(true);
-    indexText.setWidthFull();
-
+  private HorizontalLayout buildInfoRow(ApiResponse response) {
     var statusText = new TextField("Status", response.statusCode().toString(), "");
     statusText.setReadOnly(true);
     statusText.setWidthFull();
@@ -61,12 +64,12 @@ public class ResultDetail extends Dialog {
     responseTime.setWidthFull();
 
     // index, status, and response time on the same row
-    var infoRow = new HorizontalLayout(indexText, statusText, responseTime);
+    var infoRow = new HorizontalLayout(statusText, responseTime);
     infoRow.setWidthFull();
     infoRow.setSpacing(true);
     infoRow.setPadding(false);
-    infoRow.setAlignItems(Alignment.STRETCH);
-    infoRow.setFlexGrow(1, indexText, statusText, responseTime);
+    infoRow.setAlignItems(STRETCH);
+    infoRow.setFlexGrow(1, statusText, responseTime);
 
     return infoRow;
   }
@@ -84,7 +87,7 @@ public class ResultDetail extends Dialog {
     var container = new VerticalLayout(closeButton, infoRow, responseBody);
     container.setWidthFull();
     container.setSpacing(false);
-    container.getStyle().set("padding", "var(--lumo-space-xs)");
+    container.getStyle().set("padding", "var(--lumo-space-s)");
 
     return container;
   }
