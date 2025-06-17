5b9bc27af8f3de048bc7547f6795f156df7dcfe5
Dario <dario.mauri9@gmail.com>
2025-06-17 13:15:03 +0200

    Add option to run the app via electron

--- src/main/java/com/dario/ast/view/route/MainView.java
+++ src/main/java/com/dario/ast/view/route/MainView.java
@@ -29,7 +29,6 @@ public class MainView extends VerticalLayout {
 
   // TODO bug: when creating/editing an entry in EntriesSection, focus is lost (major redesign needed)
   // TODO mappers for all domain-entity
-  // TODO electron app
 
   private final HeadlineLayout headlineLayout;
   private final MainSidebar mainSidebar;
